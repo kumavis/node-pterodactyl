9aaacf63ff6c6ed817694ae46feaf29ce1cc832f
aaron <a.aa.akio.aaron@gmail.com>
2013-08-30 18:38:46 -0700

    pretty, zoomable fractals

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 //deps
 var d3 = require('d3')
+var copyHash = require('hashish').copy
+var enableSVGPan = require('./SVGPan.js')
 
 // constants
-var vis = d3.select("body").append("svg"),
+var vis = d3.select('body').append('svg').attr('id','vizContainer')
+                           .append('g').attr('id','viewport'),
     pi = Math.PI;
+
+enableSVGPan(document.getElementById('vizContainer'))
 
 var RecArc = window.RecArc = module.exports = {
 
@@ -13,12 +18,19 @@ var RecArc = window.RecArc = module.exports = {
 
   draw: function() {
     var index = 0,
-        circles = 50
+        circles = 8,
+        circlesPerRow = 4,
+        complexityStep = 1,
+        radius = 120,
+        spacing = radius*3
     while (index < circles) {
       this.drawSprout({
-        x: 200*(index%6)+100,
-        y: 200*Math.floor(index/6)+100,
-        seperations: 1+index,
+        depth: 4,
+        x: spacing*(index%circlesPerRow)+spacing/2,
+        y: spacing*Math.floor(index/circlesPerRow)+spacing/1.5,
+        segments: index*complexityStep,
+        radius: radius,
+        startAngle: 0,
       })
       index++;
     }
@@ -26,43 +38,119 @@ var RecArc = window.RecArc = module.exports = {
 
   drawSprout: function(params) {
     var self = this,
-        segments = params.seperations
         index = 0,
-        circleRadius = 60,
-        stemLength = 25
-    while (index < segments) {
-      var startAngle = index*(360/segments)*(pi/180),
-          endAngle = (index+1-0.1)*(360/segments)*(pi/180),
-          innerRadius = circleRadius,
-          outerRadius = innerRadius+5
+        segments = params.segments+1, //+1 to give space for root
+        outerDomainRadius = params.radius,
+        coreDomainRadius = params.radius/3,
+        //childRadius = params.radius/3,
+        zeta = Math.sin(2*pi/segments),
+        childRadius = (2*zeta*coreDomainRadius)/(2*(1-zeta)),
+        maxChildRadius = (outerDomainRadius-coreDomainRadius)/2
 
+    // apply max to radius
+    // if (childRadius>maxChildRadius) {
+    //   console.log('squeeze radius: '+segments)
+    //   childRadius = maxChildRadius 
+    // }
+    childRadius = maxChildRadius 
+
+    // if (segments>6) {
+    //   childRadius = (2*zeta*coreDomainRadius)/(2*(1-zeta))
+    //   // var outerDomainRadius = (2*zeta*innerRadius)/(2*(1-zeta))
+    //   console.log('depth: '+params.depth)
+    //   console.log('segments: '+segments)
+    //   console.log('zeta: '+zeta)
+    //   console.log('outerDomainRadius: '+outerDomainRadius)
+    // }
+
+    while (index < segments) {
+      var startAngle = index*(360/segments)*(pi/180)+params.startAngle,
+          endAngle = (index+1-0.1)*(360/segments)*(pi/180)+params.startAngle
+
+      // // Draw Core
       self.drawArc({
         x: params.x,
         y: params.y,
-        innerRadius: innerRadius,
-        outerRadius: outerRadius,
+        innerRadius: coreDomainRadius,
+        outerRadius: coreDomainRadius*1.1,
         startAngle: startAngle,
         endAngle: endAngle,
       })
+
+      var childOrigin = {
+        x: params.x+(coreDomainRadius+childRadius)*Math.sin(startAngle),
+        y: params.y+(coreDomainRadius+childRadius)*-Math.cos(startAngle),
+      }
+      
+      // Draw Stem
+      var stem = {
+        innerPoint: {
+          x: params.x+coreDomainRadius*Math.sin(startAngle),
+          y: params.y+coreDomainRadius*-Math.cos(startAngle),
+        },
+        outerPoint: {
+          x: params.x+(coreDomainRadius+childRadius/1.5)*Math.sin(startAngle),
+          y: params.y+(coreDomainRadius+childRadius/1.5)*-Math.cos(startAngle),
+        },
+      }
       self.drawLine({
-        x1: params.x+innerRadius*Math.sin(startAngle),
-        y1: params.y+innerRadius*-Math.cos(startAngle),
-        x2: params.x+(innerRadius+stemLength)*Math.sin(startAngle),
-        y2: params.y+(innerRadius+stemLength)*-Math.cos(startAngle),
+        width: coreDomainRadius/50,
+        x1: stem.innerPoint.x,
+        y1: stem.innerPoint.y,
+        x2: stem.outerPoint.x,
+        y2: stem.outerPoint.y,
       })
+
+      // // DEBUGGERY - domains
+      // // draw core domain
+      // self.drawCircle({
+      //   radius: coreDomainRadius,
+      //   x: params.x,
+      //   y: params.y,
+      // }).style('fill', 'rgb(6,120,155)')
+      // // draw outer domain
+      // self.drawCircle({
+      //   radius: outerDomainRadius,
+      //   x: params.x,
+      //   y: params.y,
+      // }).style('fill', 'rgb(6,120,155)')
+
+      if (params.depth>0 && index!=0) {
+        // modify params for child
+        var newParams = copyHash(params)
+        newParams.depth--
+        newParams.x = childOrigin.x
+        newParams.y = childOrigin.y
+        newParams.radius = childRadius
+        newParams.startAngle = startAngle-pi
+        // draw child
+        self.drawSprout( newParams )
+      }
       index++;
     }
   },
 
   drawLine: function(params) {
     // var line = d3.svg.arc()
-    vis.append("svg:line")
-      .attr("x1", params.x1)
-      .attr("y1", params.y1)
-      .attr("x2", params.x2)
-      .attr("y2", params.y2)
-      .style("stroke", "rgb(6,120,155)")
-      .style("stroke-width", 2);
+    var path = vis.append('svg:line')
+      .attr('x1', params.x1)
+      .attr('y1', params.y1)
+      .attr('x2', params.x2)
+      .attr('y2', params.y2)
+      .style('stroke', 'rgb(6,120,155)')
+      .style('stroke-width', params.width);
+    return path
+  },
+
+  drawCircle: function(params) {
+    return this.drawArc({
+      innerRadius: params.radius*0.95,
+      outerRadius: params.radius,
+      startAngle: 0,
+      endAngle: 2*pi,
+      x: params.x,
+      y: params.y,
+    })
   },
 
   drawArc: function(params) {
@@ -71,10 +159,10 @@ var RecArc = window.RecArc = module.exports = {
       .outerRadius(params.outerRadius)
       .startAngle(params.startAngle) //in radians
       .endAngle(params.endAngle)     //in radians
-    vis.append("path")
-      .attr("d", arc)
-      .attr("transform", "translate("+params.x+","+params.y+")")
-    return arc
+    var path = vis.append('path')
+      .attr('d', arc)
+      .attr('transform', 'translate('+params.x+','+params.y+')')
+    return path
   },
 
 };
